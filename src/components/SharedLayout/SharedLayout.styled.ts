de070fda757fe0fb447c357fb9fb15d072a0956c
MishaPanivnyk <panivnykm@gmail.com>
2024-11-22 20:38:18 +0200

    fix base-url

--- src/components/SharedLayout/SharedLayout.styled.ts
+++ src/components/SharedLayout/SharedLayout.styled.ts
@@ -1,8 +1,6 @@
 import styled from 'styled-components';
-import BgImgPc from '../../img/background-main-pc.jpg';
 
 export const BackImg = styled.div`
-  background: url(${BgImgPc});
   background-repeat: no-repeat;
   background-size: cover;
   /* background-position: center; */
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'cryonix-frontend',
+  base: 'panivnyk-studio',
   resolve: {
     alias: {
       components: path.resolve(__dirname, 'src/components'),
